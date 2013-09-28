c4296d6be30784702914d62844e94b61ab31b97f
lemigue <leandro.idc@gmail.com>
2013-09-27 23:10:36 -0300

    Revert "primeira publicação"
    This reverts commit f8007f0e911e434e31963d5854e7cd0968e15758.

--- _references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="Scripts/jquery-2.0.3.min.js" />
-/// <reference path="Scripts/jquery-ui-1.10.3.js" />
-/// <reference path="Scripts/qunit-1.12.0.js" />
-/// <reference path="plungerjs-0.0.1.js" />
-
